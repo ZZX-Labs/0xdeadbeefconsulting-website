4464c5ecf06ce23844ef45a08c5df8132184e194
ZZX-Laboratories <zzx-laboratories@protonmail.ch>
2024-11-30 00:12:16 -0500

    Update script.js

--- website/script.js
+++ website/script.js
@@ -1,1 +1,4 @@
-
+// Optional script to dynamically add functionality (future expansions)
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Landing page script loaded successfully.");
+});
